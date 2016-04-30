5a1317373b6f2d577d6a5822e6b7d5f5418cfc22
Shi Fan <sf2632@nyu.edu>
2016-04-29 20:24:49 -0400

    update similarity functions

--- analysis/userSimilarity.py
+++ analysis/userSimilarity.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 from sklearn.metrics import jaccard_similarity_score
+from scipy.spatial.distance import cosine
 
 
 def getJaccardSimilarity(user1=None, user2=None):
@@ -26,10 +27,10 @@ def getFrequencySimilarity(user1=None, user2=None, normalize=False):
             user1 = user1 / np.sum(user1)
             user2 = user2 / np.sum(user2)
         index_to_sum = np.intersect1d(np.nonzero(user1), np.nonzero(user2))
-    return np.sum(user1[index_to_sum]) + np.sum(user2[index_to_sum]) / 2
+    return (np.sum(user1[index_to_sum]) + np.sum(user2[index_to_sum])) / 2
 
 
-def getConsineSimilarity(user1=None, user2=None):
+def getCosineSimilarity(user1=None, user2=None):
     if user1.ndim != 1 or user2.ndim != 1:
         print 'Input arrays must be 1-dimensional'
         return
@@ -37,9 +38,11 @@ def getConsineSimilarity(user1=None, user2=None):
         print 'Input arrays must have the same length'
         return
     else:
-        return jaccard_similarity_score(user1, user2)
+        return 1-cosine(user1, user2)
 
-
-user1 = [0, 1, 1, 3, 4, 2, 1]
-user2 = [3, 1, 3, 4, 1, 4, 3]
-getConsineSimilarity()
+# test cases below
+# user1 = np.array([0, 1, 1, 3, 4, 2, 1])
+# user2 = np.array([3, 1, 3, 4, 1, 4, 3])
+# print getJaccardSimilarity(user1, user2)
+# print getFrequencySimilarity(user1, user2, normalize=True)
+# print getCosineSimilarity(user1, user2)
